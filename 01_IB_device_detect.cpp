6a4e5f0337d17e0001aed0bc6e139991e67ed33f
unknown <1272813056@qq.com>
2018-10-23 12:09:26 +0800

    'changed'

--- 01_IB_device_detect.cpp
+++ 01_IB_device_detect.cpp
@@ -7,5 +7,24 @@ int main(int argc,char **argv)
 {
     MPI_Init(&argc,&argv);
 
+    int rank,size;
+    struct ibv_device ** IPH_Ib_devlist;
+    struct ibv_device * Iph_dev;
+
+    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+    IPH_Ib_devlist = ibv_get_device_list(0);
+    Iph_dev=IPH_Ib_devlist[0];
+
+    if(rank == 0)
+    {
+    	cout<<Iph_dev->node_type<<endl;
+    	cout<<Iph_dev->transport_type <<endl;
+    	cout<<ibv_get_device_name(Iph_dev)<<endl;
+    }
+
+
+    ibv_free_device_list(IPH_Ib_devlist); 
     MPI_Finalize();
 }
